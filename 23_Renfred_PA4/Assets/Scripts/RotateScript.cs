04652ac6c4a8b7c36ec8c5d8bba18bc3457c0e37
Xizuki <64124274+Xizuki@users.noreply.github.com>
2020-05-04 19:28:53 +0800

    Updated from local repository Renfred

---
+++ 23_Renfred_PA4/Assets/Scripts/RotateScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateOver1();
+    }
+
+    public void RotateOver1()
+    {
+        transform.position += new Vector3(Mathf.Sin(Time.time) * 0.0025f, 0, Mathf.Cos(Time.time) * 0.0025f);
+    }
+    public void RotateOver2()
+    {
+        transform.position += new Vector3(Mathf.Sin(Time.time)*0.02f, Mathf.Cos(Time.time) * 0.02f, 0);
+    }
+}
